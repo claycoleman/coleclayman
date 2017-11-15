66a4150f17b282c0bef3b1091c885e4194aad60a
Clay Coleman <coleclayman@gmail.com>
2017-11-15 03:45:10 -0700

    added files and methods to handle obtaining Hubspot data and sending a response back with our MatterMark data.

--- scripts/process_company_via_api.py
+++ scripts/process_company_via_api.py
@@ -25,65 +25,8 @@ django.setup()
 from ugf.models import *
 from django.conf import settings
 
-def grab_mattermark_data(company):
-    search_url = "https://api.mattermark.com/companies/%s?key=%s" % (company.mattermark_id, settings.MATTERMARK_API_KEY)
-    resp = requests.get(search_url)
-    resp = resp.json()
 
-    misses = 0
-
-    company.domain = resp.get('website')
-    company.zip_code = resp.get("zip")
-    company.state = resp.get("state")
-    company.city = resp.get("city")
-    company.country = resp.get("country")
-
-    employee_count = resp.get('employee_count')[0].get("score", "0") if resp.get('employee_count') else 0
-    company.employee_count = int(employee_count) if employee_count else None
     
-    company.total_funding = int(resp.get("total_funding")) if resp.get('total_funding') else None
-    if not company.total_funding:
-        misses += 1
-    company.last_funding_amount = int(resp.get("last_funding_amount")) if resp.get('last_funding_amount') else None
-    if not company.last_funding_amount:
-        misses += 1
-    
-    last_funding_date = resp.get('last_funding_date', None)
-    if last_funding_date:
-        last_funding_date = datetime.datetime.strptime(last_funding_date, "%Y-%m-%d")
-    
-    company.last_funding_date = last_funding_date
-    if not company.last_funding_date:
-        misses += 1
-
-    company.current_series = resp.get("stage")
-    if not company.current_series:
-        misses += 1
-
-    company.data_retrieved = True
-    company.date_data_retrieved = datetime.datetime.now()
-
-    for funding_round in resp.get("funding"):
-        amount = int( funding_round.get("amount") ) if funding_round.get("amount") else None
-        series = funding_round.get("series", "")
-        funding_date = funding_round.get("funding_date", "")
-
-        if funding_date:
-            funding_date = datetime.datetime.strptime(funding_date, "%Y-%m-%d")
-
-        new_funding_round = FundingRound.objects.create(amount=amount, series=series, funding_date=funding_date, company=company)
-        for investor in funding_round.get("investors").split(", "):
-            new_investor, created = Investor.objects.get_or_create(name=investor)
-            new_investor.companies.add(company)
-            new_investor.funding_rounds.add(new_funding_round)
-            new_investor.save()
-
-        new_funding_round.save()
-    
-    company.valid_candidate = misses < 3 and not re.search('exited', company.current_series, re.IGNORECASE)
-    company.save()
-
-        
 
 if not sys.argv[1]:
     print "usage: ./process_company_via_api.py <ugf-company-id>"
@@ -115,6 +58,7 @@ if not company.mattermark_id:
 
 # get mattermark data for company_id
 if company.mattermark_id != settings.MATTERMARK_FAIL:
-    grab_mattermark_data(company)
+    company.grab_mattermark_data()
+    company.update_hubspot_with_data()
 
 sys.exit()
--- ugf/models.py
+++ ugf/models.py
@@ -1,3 +1,10 @@
+import re
+import requests
+import datetime
+import json
+import time
+
+from django.conf import settings
 from django.db import models
 
 # hopefully things are passed in like this
@@ -47,7 +54,9 @@ class Company(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
 
     valid_candidate = models.BooleanField(default=True)
+
     data_retrieved = models.BooleanField(default=False)
+    data_pushed_to_hubspot = models.BooleanField(default=False)
     data_can_retrieved = models.BooleanField(default=True)
     date_data_retrieved = models.DateTimeField(null=True, blank=True)
 
@@ -64,6 +73,165 @@ class Company(models.Model):
 
     def get_last_funded_date(self):
         return self.last_funding_date
+
+
+    def grab_hubspot_data(self):
+        # make a request to the HubSpot db to get Company info
+        resp = requests.get("https://api.hubapi.com/companies/v2/companies/%d?hapikey=%s&userId=%s" % (self.hubspot_id, settings.HUBSPOT_HAPI_KEY, settings.HUBSPOT_USER_ID) )
+        print resp
+        resp = resp.json()
+        props = resp.get("properties", False)
+        
+        if not props:
+            # we didn't get a valid company back which is weird because we JUST got a company from them
+            return False
+        
+        # save it, then make mattermark request
+        self.name = get_hubspot_value_for_property(props, 'name')
+        self.domain = get_hubspot_value_for_property(props, 'domain')
+        self.save()
+
+        return True
+
+    def grab_mattermark_data(self):
+        search_url = "https://api.mattermark.com/companies/%s?key=%s" % (self.mattermark_id, settings.MATTERMARK_API_KEY)
+        resp = requests.get(search_url)
+        resp = resp.json()
+
+        misses = 0
+
+        self.domain = resp.get('website')
+        self.zip_code = resp.get("zip")
+        self.state = resp.get("state")
+        self.city = resp.get("city")
+        self.country = resp.get("country")
+
+        employee_count = resp.get('employee_count')[0].get("score", "0") if resp.get('employee_count') else 0
+        self.employee_count = int(employee_count) if employee_count else None
+        
+        self.total_funding = int(resp.get("total_funding")) if resp.get('total_funding') else None
+        if not self.total_funding:
+            misses += 1
+        self.last_funding_amount = int(resp.get("last_funding_amount")) if resp.get('last_funding_amount') else None
+        if not self.last_funding_amount:
+            misses += 1
+        
+        last_funding_date = resp.get('last_funding_date', None)
+        if last_funding_date:
+            last_funding_date = datetime.datetime.strptime(last_funding_date, "%Y-%m-%d")
+        
+        self.last_funding_date = last_funding_date
+        if not self.last_funding_date:
+            misses += 1
+
+        self.current_series = resp.get("stage")
+        if not self.current_series:
+            misses += 1
+
+        self.data_retrieved = True
+        self.date_data_retrieved = datetime.datetime.now()
+
+        for index, funding_round in enumerate(resp.get("funding")):
+            series = funding_round.get("series", "")
+            amount = int( funding_round.get("amount") ) if funding_round.get("amount") else None
+            funding_date = funding_round.get("funding_date", "")
+            
+            if not series or amount or funding_date:
+                continue
+
+            if funding_date:
+                funding_date = datetime.datetime.strptime(funding_date, "%Y-%m-%d")
+
+            new_funding_round = FundingRound.objects.create(amount=amount, series=series, funding_date=funding_date, company=self, funding_number=index)
+            for investor in funding_round.get("investors").split(", "):
+                new_investor, created = Investor.objects.get_or_create(name=investor)
+                new_investor.companies.add(self)
+                new_investor.funding_rounds.add(new_funding_round)
+                new_investor.save()
+
+            new_funding_round.save()
+        
+        self.valid_candidate = misses < 3 and not re.search('exited', self.current_series, re.IGNORECASE)
+        self.save()
+
+        self.update_funding_predictors()
+        
+        
+    def update_funding_predictors(self):
+        fundings = list(self.fundinground_set.order_by("funding_number"))
+        for i in xrange(0, len(fundings) - 1):
+            curr_funding = fundings[i]
+            if not fundings[i+1].funding_date and curr_funding.funding_date:
+                continue
+            rp, created = RoundPredictor.objects.get_or_create(current_series=curr_funding.series)
+            rp.add_new_data_point( (fundings[i+1].funding_date - curr_funding.funding_date).days )
+        
+    
+    def update_hubspot_with_data(self):
+        if not self.data_retrieved:
+            return 
+
+        data = {
+            "properties": []
+        }
+        if self.last_funding_date:
+            data.get('properties').append({
+                'name': "last_funding_date",
+                # timestamp in milliseconds
+                'value': long(time.mktime(self.last_funding_date.date().timetuple())) * 1000,
+            })
+
+        if self.employee_count:
+            data.get('properties').append({
+                'name': "numberofemployees",
+                'value': self.employee_count,
+            })
+
+        if self.last_funding_amount:
+            data.get('properties').append({
+                'name': "last_funding_amount",
+                'value': self.last_funding_amount,
+            })
+
+        if self.current_series:
+            data.get('properties').append({
+                'name': "stage",
+                'value': self.current_series,
+            })
+        
+        # calculate date_for_next_interaction
+        if self.current_series and self.last_funding_date:
+            number_of_days_to_add = 365 # defaults to a year
+            try: 
+                predictor = RoundPredictor.objects.get(current_series=self.current_series)
+                number_of_days_to_add = int(predictor.average_number_of_days)
+            except Exception, e:
+                pass
+
+            td = datetime.timedelta(days=number_of_days_to_add)
+           
+            date_for_next_interaction = self.last_funding_date + td
+
+            # TODO think of a way that will let UGF people simply update next date_for_next_interaction
+            # that will signal to backend that we need to remind again after a date
+            data.get('properties').append({
+                'name': "date_for_next_interaction",
+                # timestamp in milliseconds
+                'value': long(time.mktime(date_for_next_interaction.date().timetuple())) * 1000,
+            })
+            self.set_date_for_next_interaction = True
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+        print data
+
+        resp = requests.put("https://api.hubapi.com/companies/v2/companies/%d?hapikey=%s&userId=%s" % (self.hubspot_id, settings.HUBSPOT_HAPI_KEY, settings.HUBSPOT_USER_ID), data=json.dumps(data), headers=headers)
+        print resp
+        print resp.text
+        self.data_pushed_to_hubspot = True
+        self.save()
+
 
 
 class Source(models.Model):
@@ -89,6 +257,7 @@ class FundingRound(models.Model):
     series = models.CharField(max_length=255)
     funding_date = models.DateTimeField(null=True, blank=True)
     amount = models.BigIntegerField(null=True, blank=True)
+    funding_number = models.IntegerField(default=0)
 
     def __unicode__(self):
         """Unicode representation of Source."""
@@ -104,3 +273,35 @@ class Investor(models.Model):
         """Unicode representation of Source."""
         return self.name
 
+
+class RoundPredictor(models.Model):
+    current_series = models.CharField(null=True, blank=True, max_length=255)
+    data_list = models.TextField(null=True, blank=True)
+    average_number_of_days = models.FloatField(default=0)
+    number_of_data_points = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        """Unicode representation of Source."""
+        return "series %s waits %.2f on average" % (self.current_series, self.average_number_of_days)
+
+    def add_new_data_point(self, number_of_days):
+        if self.number_of_data_points > 0:
+            self.data_list += ";%d" % number_of_days
+            full_weight = self.full_weight
+            full_weight += number_of_days
+            self.number_of_data_points += 1
+            self.average_number_of_days = full_weight / self.number_of_data_points
+        else:
+            self.data_list = "%d" % number_of_days
+            self.number_of_data_points = 1
+            self.average_number_of_days = number_of_days
+
+        self.save()
+
+    @property
+    def full_weight(self):
+        return self.average_number_of_days * self.number_of_data_points
+
+
+def get_hubspot_value_for_property(props, property_name):
+    return props.get(property_name, {}).get('value', None)
